20b2439554d8578546180abe071de2542b683465
AlexandreCrevel <94857674+AlexandreCrevel@users.noreply.github.com>
2024-12-18 18:48:27 +0100

    ✨ add shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,6 @@
-import {
-  ClerkProvider,
-  SignIn,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs';
+import { AppSidebar } from '@/components/app-sidebar';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { ClerkProvider, SignIn, SignedIn, SignedOut } from '@clerk/nextjs';
 import { shadesOfPurple } from '@clerk/themes';
 import './globals.css';
 export default function RootLayout({
@@ -21,15 +17,20 @@ export default function RootLayout({
     >
       <html lang='en'>
         <body className='w-full h-full justify-center items-center'>
-          <SignedOut>
-            <div className='flex justify-center items-center h-screen w-full'>
-              <SignIn forceRedirectUrl='/dashboard' />
-            </div>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-            {children}
-          </SignedIn>
+          <SidebarProvider>
+            <SignedOut>
+              <div className='flex justify-center items-center h-screen w-full'>
+                <SignIn forceRedirectUrl='/dashboard' />
+              </div>
+            </SignedOut>
+            <SignedIn>
+              <AppSidebar />
+              <main>
+                <SidebarTrigger />
+                {children}
+              </main>
+            </SignedIn>
+          </SidebarProvider>
         </body>
       </html>
     </ClerkProvider>
